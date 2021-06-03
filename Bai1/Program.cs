59b2f32b1da65c21a5a51200792bbefbc921fc05
huy <huy@gmail.com>
2021-06-03 08:38:51 +0700

    lab1

---
+++ Bai1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using Bai1;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Ex1 ex1 = new Ex1();
+            Ex1.ThongTin();
+        }
+    }
+}
---
+++ Bai1/ThongTin.cs
@@ -0,0 +1,9 @@
+﻿namespace Bai1
+{
+    public class ThongTin
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
