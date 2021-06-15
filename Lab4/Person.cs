d2e35460ebec64eebfa93da4e3b48d046e161f3c
anhhuyprox6qm <73454962+anhhuyprox6qm@users.noreply.github.com>
2021-06-15 21:16:21 +0700

    Update Person.cs

--- Lab4/Person.cs
+++ Lab4/Person.cs
@@ -1,15 +1,13 @@
-﻿namespace Lab4
+namespace Lab4
 {
     public class Person
     {
         public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; }
-
         public override string ToString()
         {
-            return $"Name: {Name}, Phone: {Phone}, Address: {Address}, Email: {Email}";
+            return $"Name : {Name} - Email : {Email}";
         }
     }
 }
