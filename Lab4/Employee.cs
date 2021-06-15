197f9ad3cb824b68570c02e7746c96e5bef443ac
anhhuyprox6qm <73454962+anhhuyprox6qm@users.noreply.github.com>
2021-06-15 21:15:01 +0700

    Update Employee.cs

--- Lab4/Employee.cs
+++ Lab4/Employee.cs
@@ -1,17 +1,11 @@
-﻿namespace Lab4
+namespace Lab4
 {
     public abstract class Employee: Person
     {
         public string Department { get; set; }
-        public double Salary { set; get; }
-        public string DateHied { set; get; }
-
-        public override string ToString()
-        {
-            return $"Name: {Name}, Phone: {Phone}, Address: {Address}, Email: {Email}, Department: {Department}, Salary: {Salary}, DateHied: {DateHied}";
-        }
-
-        public abstract double CalculBonus();
-        public abstract void CalculateVacation();
+        public double Salary { get; set; }
+        public int DateHired { get; set; }
+        public abstract int CalculateVacation();
+        public abstract double CalculateBonus();
     }
 }
